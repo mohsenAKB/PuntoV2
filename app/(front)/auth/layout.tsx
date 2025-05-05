abf2feaef2ee5ff4d004b962f9c81b55872f0402
mohsenAKB <mohsen.akb1377@gmail.com>
2025-05-05 23:19:23 +0330

    [REFACTOR]:refactor style

---
+++ app/(front)/auth/layout.tsx
@@ -0,0 +1,13 @@
+import { AuthLayoutProps } from "@/@types/layout/auth-layout";
+import AuthenticationLayout from "@/components/Layout/AuthLayout/AuthLayout";
+import { FC, JSX } from "react";
+
+const AuthLayout: FC<AuthLayoutProps> = (props): JSX.Element => {
+  const { children, header } = props;
+
+  return (
+    <AuthenticationLayout header={header}>{children}</AuthenticationLayout>
+  );
+};
+
+export default AuthLayout;
