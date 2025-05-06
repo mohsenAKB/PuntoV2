d3b8cf924a987e54edae73f79c4ef5a77b067a53
mohsenAKB <mohsen.akb1377@gmail.com>
2025-05-06 18:42:52 +0330

    [CREATE]:refactor header

--- store/store.ts
+++ store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./slices/authSlice";
+import { configsSlice } from "./slices/configsSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    config: configsSlice.reducer,
   },
   devTools: process.env.NEXT_PUBLIC_MODE === "development",
 });
